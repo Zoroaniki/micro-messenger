ed7f3670856b181d3340e4eb1b69c346aff4b804
ZoroAniki <mr.nexus2016@mail.ru>
2024-04-23 01:53:15 +0300

    Add

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, session, jsonify
 import pymysql
 import mysql.connector
+import requests
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
+
 
 # Настройка подключения к базе данных
 def get_db():
     try:
         connection = mysql.connector.connect(host='localhost',
-                                        port='3306',
-                                        database='users',
-                                        user='root',
-                                        password='250702')
+                                             port='3306',
+                                             database='users',
+                                             user='root',
+                                             password='250702')
         if connection.is_connected():
             db_Info = connection.get_server_info()
             print("Подключено к серверу MySQL версии", db_Info)
@@ -32,10 +34,10 @@ def get_db():
             print("Соединение с MySQL закрыто")
     '''
 
-@app.route('/')
+
+@app.route('/success')
 def index():
     return render_template('index.html')
-
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -43,9 +45,10 @@ def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
+        number = request.form['number']
         conn = get_db()
         cursor = conn.cursor()
-        cursor.execute("SELECT * FROM users WHERE name = %s AND pass = %s", (username, password))
+        cursor.execute("SELECT * FROM users WHERE name = %s AND pass = %s AND number = %s", (username, password, number))
         user = cursor.fetchone()
         if user:
             session['username'] = username
@@ -54,12 +57,25 @@ def login():
             return 'Invalid username or password'
     return render_template('login.html')
 
-'''
-@app.route('/logout')
-def logout():
-    session.pop('username', None)
-    return redirect(url_for('index'))
-'''
+
+@app.route('/users/<id>', methods=['GET', 'POST'])
+def ilya(id: int):
+    conn = get_db()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users WHERE id = {} ".format(id))
+    user = cursor.fetchone()
+    print(user)
+    id, name, password, number = user
+    return name
+
+
+def send_request(url="http://127.0.0.1:5000/poisk"):
+    response = requests.get(url="http://127.0.0.1:5000/poisk")
+    print(response)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -67,112 +83,14 @@ def register():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
+        number = request.form['number']
         conn = get_db()
         cursor = conn.cursor()
-        cursor.execute("INSERT INTO users (name, pass) VALUES (%s, %s)", (username, password))
+        cursor.execute("INSERT INTO users (name, pass, number) VALUES (%s, %s, %s)", (username, password, number))
         conn.commit()
         return redirect(url_for('login'))
-    return render_template('login.html')
-
-'''
-if __name__ == '__main__':
-    app.run(debug=True)
-
-'''
-
-'''
-import sqlalchemy as SQLAlchemy
-from flask import Flask, render_template, request, redirect, url_for
-from sqlalchemy import create_engine
-
-app = Flask(__name__)
-engine = create_engine("mysql://root:250702@localhost/users")
-db = SQLAlchemy
-
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(120), nullable=False)
-
-
-@app.route('/')
-def home():
     return render_template('register.html')
 
 
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        new_user = User(username=username, password=password)
-        try:
-            db.session.add(new_user)
-            db.session.commit()
-            return redirect(url_for('login'))
-        except:
-            return 'There was an issue adding your account'
-    else:
-        return render_template('register.html')
-
-
-@app.route('/login')
-def login():
-    return "Login Page"
-
-
-def writedb():
-    db = mysql.connector.connect(
-        host="localhost",
-        port="3306",
-        user="root",
-        password="250702",
-        database="users"
-    )
-
-    # Создание курсора
-    cursor = db.cursor()
-
-    # SQL запрос для создания таблицы
-    sql_create_table =
-    CREATE TABLE IF NOT EXISTS users (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        name VARCHAR(255) NOT NULL,
-        email VARCHAR(255) NOT NULL UNIQUE
-    );
-
-
-    # Выполнение запроса
-    cursor.execute(sql_create_table)
-
-    # Подтверждение изменений
-    db.commit()
-
-    print("Таблица создана успешно.")
-
-    # Подготовка данных для вставки
-    data = [
-        ("John Doe", "25asd"),
-        ("Jane Doe", "26asd"),
-        ("Denis", "qwerty321")
-    ]
-
-    # SQL запрос для вставки данных
-    sql_insert_data = "INSERT INTO users (name, pass) VALUES (%s, %s)"
-
-    # Выполнение запроса для каждой строки данных
-    cursor.executemany(sql_insert_data, data)
-
-    # Подтверждение изменений
-    db.commit()
-
-    print(cursor.rowcount, "записей вставлено.")
-
-    cursor.close()
-    db.close()
-'''
-'''
 if __name__ == '__main__':
-    app.run(debug=True)
-'''
+    app.run(debug=True, host="192.168.0.141")
