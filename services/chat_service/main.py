dfb8f297cb2d9291d8cd34f7a1ffc71187dcb1b4
IlfiDev <ilyafillipov2@gmail.com>
2024-04-24 22:03:50 +0300

    change docker-compose.yml

--- services/chat_service/main.py
+++ services/chat_service/main.py
@@ -5,9 +5,16 @@ from models.message import Message
 from fastapi import FastAPI
 from endpoints.chat_router import urls_blueprint
 from db.database import db
+from dotenv import load_dotenv, dontenv_values
+import os
 
+load_dotenv()
+
+user = os.getenv("BD_USER_NAME")
+password = os.getenv("BD_PASSWORD")
+bd_name = os.getenv("BD_NAME")
 app = Flask(__name__, template_folder='templates')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://ilfi:password@localhost/chat_service_db?charset=utf8mb4&collation=utf8mb4_general_ci'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://{user}:{password}@localhost/{bd_name}?charset=utf8mb4&collation=utf8mb4_general_ci'
 db.init_app(app)
 app.register_blueprint(urls_blueprint, url_prefix='/api')
 
@@ -15,4 +22,4 @@ with app.app_context():
     db.create_all()
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8000, debug=True)
+    app.run(host="0.0.0.0", port=8000, debug=True)
