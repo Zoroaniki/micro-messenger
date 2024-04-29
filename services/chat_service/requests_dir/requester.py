3e47d68e09f2b68571017648d0e31482ee05a89c
IlfiDev <ilyafillipov2@gmail.com>
2024-04-29 19:10:52 +0300

    add some more forgotten imports

--- services/chat_service/requests_dir/requester.py
+++ services/chat_service/requests_dir/requester.py
@@ -1,13 +1,20 @@
 import requests
+import os
+from dotenv import load_dotenv, dotenv_values
+
+
+redirect_address = os.getenv("REDIRECT_ADDRESS")
+
+load_dotenv()
 
 def request_name(user_id: int):
     print("______________________________________________________________________")
     print(user_id)
-    response = requests.get("http://0.0.0.0:8001/users/{}".format(user_id))
+    response = requests.get("{}:8001/users/{}".format(redirect_address, user_id))
     print(response)
     #print(response.json())
     return response.text
 
 def request_uuid(user_id: int):
-    response = requests.get("http://0.0.0.0:8002/user/{}/uuid".format(user_id))
+    response = requests.get("{}:8002/user/{}/uuid".format(redirect_address, user_id))
     return response.text
