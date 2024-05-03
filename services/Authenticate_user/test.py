0c6b68b307ff5243fce4da83413df791fcbf899e
IlfiDev <ilyafillipov2@gmail.com>
2024-05-03 14:14:50 +0300

    change test

--- services/Authenticate_user/test.py
+++ services/Authenticate_user/test.py
@@ -1,7 +1,7 @@
 # для app.py аунтификация
 import unittest
 from unittest.mock import patch
-from app import app, get_db, send_request
+from serviecs.Authenticate_user.app import app, get_db, send_request
 
 
 class TestApp(unittest.TestCase):
